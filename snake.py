1b0b334c86c4af3d8ec3be6effb4b0909a5d1abe
daniel <294.daniel@gmail.com>
2021-10-13 11:09:08 +0200

    Created snake with snake_size number of segments

--- snake.py
+++ snake.py
@@ -5,14 +5,25 @@ src.setup(height=600, width=600)
 src.bgcolor('black')
 src.title('Snake Game')
 
-snake = [Turtle() for i in range(3)]
-for i in range(3):
-	block = snake[i]
-	block.shape("square")
-	block.color('white')
-	# default turtle size is 20px
-	block.penup()
+# create three segments of turtle
+snake_size = 3
+
+# class Block(Turtle):
+# 	def __init__(self):
+# 		self.shape('square')
+# 		self.color('white')
+# 		self.penup()
+# 		self.posx = 0
+# 		self.posy = 0
+# 		self.pos = [self.posx, self.posy]
+
+
+snake = [Turtle() for i in range(snake_size)]
+for i in range(snake_size):
+	snake[i].shape('square')
+	snake[i].color('white')
+	snake[i].penup()
 	# move each block 20px backwards
-	block.setpos(-20*i,0)
+	snake[i].goto(-20*i,0)
 
 src.exitonclick()
