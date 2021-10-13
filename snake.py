064c8f144cddd30ce3a6b0c64eac1ed0c20c30c4
daniel <294.daniel@gmail.com>
2021-10-13 12:04:57 +0200

    Snake moves rightwards and move_vertical is implemented but still not checked

--- snake.py
+++ snake.py
@@ -1,9 +1,11 @@
+import time
 from turtle import Turtle, Screen
 
-src = Screen()
-src.setup(height=600, width=600)
-src.bgcolor('black')
-src.title('Snake Game')
+scr = Screen()
+scr.setup(height=600, width=600)
+scr.bgcolor('black')
+scr.title('Snake Game')
+scr.tracer(0)
 
 # create three segments of turtle
 snake_size = 3
@@ -19,11 +21,57 @@ snake_size = 3
 
 
 snake = [Turtle() for i in range(snake_size)]
+snake_positions = []
+orientation = 'E'
+
 for i in range(snake_size):
 	snake[i].shape('square')
 	snake[i].color('white')
 	snake[i].penup()
 	# move each block 20px backwards
-	snake[i].goto(-20*i,0)
+	snake[i].goto(-20*i, 0)
 
-src.exitonclick()
+
+def move_snake():
+	while True:
+		scr.update()
+		for i in snake:
+			i.forward(20)
+		time.sleep(0.1)
+
+def move_vertical(direction):
+	# can move upwards only if moving horizontally
+	if direction in ['E', 'W']:
+
+		head = snake[0]
+		head_pos_x = head.position()[0]
+
+		# set turning direction of snake head
+		if direction == 'E':
+			head.setheading(90)
+		elif direction == 'W':
+			head.setheading(0)
+		head.forward(20)
+
+		# move rest of snake segments
+		while snake[-1].position != head.pos:
+			for i in snake[1:]:
+
+				# check if segment is at turning point
+				if i.position()[0] != head_pos_x:
+					i.forward(20)
+
+				# turn segment direction at turning point
+				else:
+
+					if direction == 'E':
+						i.setheading(90)
+					elif direction == 'W':
+						i.setheading(0)
+
+					i.forward(20)
+
+
+move_snake()
+
+scr.exitonclick()
