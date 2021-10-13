d429dee14d6b78df2a611aec243442098884028f
daniel <294.daniel@gmail.com>
2021-10-13 14:00:29 +0200

    Working on redirection of snake when reaching canvas limits
    Directions 'E,W,S,N' need to be recoded to turtle.heading() and turtle.setheading()

--- snake.py
+++ snake.py
@@ -22,12 +22,50 @@ snake_size = 3
 
 snake = [Turtle() for i in range(snake_size)]
 snake_positions = []
-direction = 'E'
+
+half_width = scr.screensize()[0] / 2
+half_height = scr.screensize()[1] / 2
+
+max_posx = half_width - 20
+min_posx = - half_width + 20
+
+max_posy = half_height -20
+min_posy = - half_height +20
+
+# West  = setheading(0)
+# East  = setheading(180)
+# North = setheading(90)
+# South = setheading(270)
 
 
 def move_snake():
+
 	for i in snake:
-		i.forward(20)
+		posx = i.position()[0]
+		posy = i.position()[1]
+		direction = i.heading()
+		if direction in [180, 0]:
+			# return to left margin of canvas
+			if direction == 0 and posx == max_posx:  # going righwards
+				i.goto(min_posx, posy)
+			# return to right margin of canvas
+			elif direction == 180 and posx == min_posx:  # going leftwards
+				i.goto(max_posx, posy)
+			# limits not reached, just move on
+			else:
+				i.forward(20)
+
+		if direction in [90, 270]:
+			# return to left margin of canvas
+			if direction == 90 and posy == max_posy:  # going upwards
+				i.goto(posx, min_posy)
+			# return to right margin of canvas
+			elif direction == 270 and posy == min_posy:  # going downwards
+				i.goto(posx, max_posyposy)
+			# limits not reached, just move on
+			else:
+				i.forward(20)
+
 	scr.update()
 	time.sleep(0.1)
 
