68e051f9df78c8e042ac7a7e8f05b5deaabb0054
daniel <294.daniel@gmail.com>
2021-10-19 11:46:57 +0200

    turn method coded

--- snake.py
+++ snake.py
@@ -7,11 +7,6 @@ scr.bgcolor('black')
 scr.title('Snake Game')
 scr.tracer(0)
 
-# create three segments of turtle
-snake_size = 4
-
-snake = [Turtle() for i in range(snake_size)]
-
 # Dimensions of canvas
 width = 260
 height = 260
@@ -42,36 +37,131 @@ x.hideturtle()
 # North = setheading(90)
 # South = setheading(270)
 
+# define the segment object
 
-def get_orientation(snk):
-	head = snk[0]
-	prehead = snk[1]
-	head_x = head.position()[0]
-	head_y = head.position()[1]
-	prehead_x = prehead.position()[0]
-	prehead_y = prehead.position()[1]
-	x_axis = 0
-	y_axis = 0
-	# moving vertical
-	if head_x == prehead_x:
+class Segment(Turtle, posx, posy):
+	def __init__(self):
+		super().color('blue')
+		super().shape('square')
+		self.x_direct = +1
+		self.y_direct = 0
+		self.posx = posx
+		self.posy = posy
+		super().penup()
+		self.goto(self.posx, self.posy)
 
-		if head_y > prehead_y:
-			y_axis = +1
-			return [x_axis, y_axis]
-		elif head_y < prehead_y:
-			y_axis = -1
-			return [x_axis, y_axis]
-	# moving horizontal
-	elif head_y == prehead_y:
+	def cross_margin(self, side):
+		if side == 'left':
+			posx = max_posx -10
+			self.goto(self.posx, self.posy)
+		elif side == 'right':
+			posx = - max_posx + 10
+			self.goto(self.posx, self.posy)
+		elif side == 'up':
+			self.posy = - max_posy + 10
+			self.goto(self.posx, self.posy)
+		elif side == 'down':
+			self.posy = max_posy - 10
+			self.goto(self.posx, self.posy)
+		
+	def turn(self, direction, key):
+		# HLWU: moving horizontally LEFTWARDS and then turn UP
+		# HLWU: moving horizontally LEFTWARDS and then turn DOWN
+		# HRWU: moving horizontally RIGHTWARDS and then turn UP
+		# HRWD: moving horizontally RIGHTWARDS and then turn DOWN
 
-		if head_x > prehead_x:
-			x_axis = +1
-			return [x_axis, y_axis]
-		elif head_x < prehead_x:
-			x_axis = -1
-			return [x_axis, y_axis]
-	else:
-		return [0, 0]
+		# VUWL: moving vertically UPWARDS and then turn LEFT
+		# VUWR: moving vertically UPWARDS and then turn RIGHT
+		# VDWL: moving vertically DOWNWARDS and then turn LEFT
+		# VDWR: moving vertically DOWNWARDS and then turn RIGHT
+
+		# movements = {'HLWU': ['R', (0, -1)], 'HLWD': ['L', (0, -1)],
+		# 			'HRWU': ['L', (0, 1)], 'HRWD': ['R', (0, 1)],
+		# 			'VUWL': ['L', (-1, 0)], 'VUWR': ['R', (1, 0)],
+		# 			'VDWL': ['R', (-1, 0)],  'VDWR': ['L', (1, 0)],
+		# 			}
+
+		# moving horizontal
+		if self.y_direct == 0:
+			# moving left
+			if self.x_direct == -1:  # leftwards
+				if key == 'up':
+					self.right(90)
+					self.x_direct = 0
+					self.y_direct = +1
+				elif key == 'down':
+					self.left(90)
+					self.x_direct = 0
+					self.y_direct = -1
+			# moving right
+			elif self.x_direct == +1:  # right
+				if key == 'up':
+					self.left(90)
+					self.x_direct = 0
+					self.y_direct = +1
+				elif key == 'down':
+					self.right(90)
+					self.x_direct = 0
+					self.y_direct = +1
+
+		# moving vertical
+		elif self.x_direct == 0:
+			# moving up
+			if self.y_direct == 1:  # up
+				if key == 'left':
+					self.left(90)
+					if self == snake[0]:
+						self.x_direct = -1
+						self.y_direct = 0
+				elif key == 'right':
+					self.right(90)
+					if self == snake[0]:
+						self.x_direct = +1
+						self.y_direct = 0
+			# moving down
+			elif self.y_direct == -1:  # down
+				if key == 'left':
+					self.right(90)
+					self.x_direct = -1
+					self.y_direct = 0
+				elif key == 'right':
+					self.left(90)
+					self.x_direct = 1
+					self.y_direct = 0
+
+# create three segments of turtle
+snake_size = 4
+
+snake = [Turtle() for i in range(snake_size)]
+# def get_orientation(snk):
+# 	head = snk[0]
+# 	prehead = snk[1]
+# 	head_x = head.position()[0]
+# 	head_y = head.position()[1]
+# 	prehead_x = prehead.position()[0]
+# 	prehead_y = prehead.position()[1]
+# 	x_axis = 0
+# 	y_axis = 0
+# 	# moving vertical
+# 	if head_x == prehead_x:
+# 
+# 		if head_y > prehead_y:
+# 			y_axis = +1
+# 			return [x_axis, y_axis]
+# 		elif head_y < prehead_y:
+# 			y_axis = -1
+# 			return [x_axis, y_axis]
+# 	# moving horizontal
+# 	elif head_y == prehead_y:
+# 
+# 		if head_x > prehead_x:
+# 			x_axis = +1
+# 			return [x_axis, y_axis]
+# 		elif head_x < prehead_x:
+# 			x_axis = -1
+# 			return [x_axis, y_axis]
+# 	else:
+# 		return [0, 0]
 
 
 def move_to_margin(block, side):
@@ -90,27 +180,7 @@ def move_snake():
 		posx = i.position()[0]
 		posy = i.position()[1]
 
-		if y_direc == 0:  # moving horizontal
-			# return to left margin of canvas
-			if x_direc == +1 and posx >= max_posx:  # going rightwards
-				move_to_margin(i, (-width, posy))
-				continue
-			# return to right margin of canvas
-			elif x_direc == -1 and posx <= min_posx:  # going leftwards
-				move_to_margin(i, (+width, posy))
-				continue
-			# limits not reached, just move on
-
-		elif x_direc == 0:  # moving vertical
-			# return to bottom margin of canvas
-			if y_direc == +1 and posy >= max_posy:  # going upwards
-				move_to_margin(i, (posx, -height))
-				continue
-			# return to right margin of canvas
-			elif y_direc == -1 and posy <= min_posy:  # going downwards
-				move_to_margin(i, (posx, +height))
-				continue
-			# limits not reached, just move on
+		
 
 		i.forward(20)
 
@@ -138,56 +208,13 @@ def turn_segment(segment, direct, key):
 	x_direct = direct[0]
 	y_direct = direct[1]
 
-	# moving horizontal
-	if y_direct == 0:
-		# moving left
-		if x_direct == -1:  # leftwards
-			if key == 'up':
-				segment.right(90)
-				if segment == snake[0]:
-					direct = [0, +1]
-			elif key == 'down':
-				segment.left(90)
-				if segment == snake[0]:
-					direct = [0, -1]
-		# moving right
-		elif x_direct == +1:  # right
-			if key == 'up':
-				segment.left(90)
-				if segment == snake[0]:
-					direct = [0, +1]
-			elif key == 'down':
-				segment.right(90)
-				if segment == snake[0]:
-					direct = [0, -1]
-	# moving vertical
-	elif x_direct == 0:
-		# moving up
-		if y_direct == 1:  # up
-			if key == 'left':
-				segment.left(90)
-				if segment == snake[0]:
-					direct = [-1, 0]
-			elif key == 'right':
-				segment.right(90)
-				if segment == snake[0]:
-					direct = [+1, 0]
-		# moving down
-		elif y_direct == -1:  # down
-			if key == 'left':
-				segment.right(90)
-				if segment == snake[0]:
-					direct = [-1, 0]
-			elif key == 'right':
-				segment.left(90)
-				if segment == snake[0]:
-					direct = [+1, 0]
+
 	return direct
 
 
 def move_up():
 	# can move upwards only if moving horizontally
-	#direc = get_orientation(snake)
+	# direc = get_orientation(snake)
 	y_direc = direction[1]
 	head_pos = snake[0].position()
 	if y_direc == 0:  # moving horizontal
@@ -238,7 +265,7 @@ def move_left():
 			for i in snake:
 				# check if segment is at turning point
 				if i.position() == head_pos:
-					direction  = turn_segment(i, direc, 'left')
+					direction = turn_segment(i, direc, 'left')
 				# move forward once it has turned or not
 				i.forward(20)
 				if i == snake[-1] and step == (required_steps - 1):
